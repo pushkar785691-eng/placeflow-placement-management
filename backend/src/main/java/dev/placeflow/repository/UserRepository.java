7565194ec2bc059e88d3ed9f3d40dd26f9b6e47a
pushkar785691-eng <pushkar785691@gmail.com>
2026-09-01 15:22:54 +0530

    chore: add backend/src/main/java/dev/placeflow/repository/UserRepository.java

---
+++ backend/src/main/java/dev/placeflow/repository/UserRepository.java
@@ -0,0 +1,5 @@
+package dev.placeflow.repository;
+import dev.placeflow.model.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface UserRepository extends JpaRepository<User,Long>{ Optional<User> findByEmailIgnoreCase(String email); boolean existsByEmailIgnoreCase(String email); }
