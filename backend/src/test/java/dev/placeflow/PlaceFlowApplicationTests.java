931906a7eb9549ffe2fd7acd5df1f79419471696
pushkar785691-eng <pushkar785691@gmail.com>
2026-09-01 15:23:34 +0530

    chore: add backend/src/test/java/dev/placeflow/PlaceFlowApplicationTests.java

---
+++ backend/src/test/java/dev/placeflow/PlaceFlowApplicationTests.java
@@ -0,0 +1,3 @@
+package dev.placeflow;
+import org.junit.jupiter.api.Test;import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest class PlaceFlowApplicationTests{@Test void contextLoads(){}}
