f3759ebe0771f87b7ec8a729f07e529b4ed4c5e1
pushkar785691-eng <pushkar785691@gmail.com>
2026-09-01 15:22:39 +0530

    chore: add backend/src/main/java/dev/placeflow/PlaceFlowApplication.java

---
+++ backend/src/main/java/dev/placeflow/PlaceFlowApplication.java
@@ -0,0 +1,9 @@
+package dev.placeflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceFlowApplication {
+  public static void main(String[] args) { SpringApplication.run(PlaceFlowApplication.class, args); }
+}
